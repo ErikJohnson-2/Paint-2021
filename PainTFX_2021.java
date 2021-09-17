466b378010c4bb84b51e3f4bdd71de901dad3163
Erik Johnson <70307312+ErikJohnson-2@users.noreply.github.com>
2021-09-16 22:09:29 -0500

    Add files via upload
    1.1

--- PainTFX_2021.java
+++ PainTFX_2021.java
@@ -1,19 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pain.t.fx_2021;
 
-
+//Imports
+import java.io.File;
+import java.net.UnknownHostException;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 /**
  *
@@ -21,50 +23,90 @@ import javafx.stage.Stage;
  */
 public class PainTFX_2021 extends Application {
     
-    //global variables
-    Image image;
-    String fileDest;
-    Double imageWidth;
-    Double imageHeight;
-    ScrollPane sp;
-    ScrollPane windowSp;
-    Scene scene;
-    VBox vbox;
-    ImageView iv1;
-    MenuPain mb;
-    
+    //global variables, referenced by other classes
+    //UX
+    static ScrollPane sp;
+    static ScrollPane windowSp;
+    static VBox vbox;
+    static HBox hbox;
+    //Image variables
+    static GraphicsContext gc;
+    static Canvas canvas;
+    //File variables
+     static File outFile;
+    static String fileDest;
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws UnknownHostException {
         
         
+        //throw an error if user tries to save before using the filefinder dialogue
+        fileDest = "null";
         
-        vbox = new VBox();
-        windowSp = new ScrollPane();
-        windowSp.setPrefSize(500, 500);
+        //initialize canvas and graphics context
+        Double imageWidth = 300.0;
+        Double imageHeight = 300.0;
+        canvas = new Canvas(imageWidth, imageHeight);
+        gc = canvas.getGraphicsContext2D();
+        
+        //initialize scrollpane
         sp = new ScrollPane();
-        imageWidth = 400.0;
-        imageHeight = 400.0;
-        iv1 = new ImageView();
-        mb = new MenuPain(primaryStage, iv1);
-        sp.setContent(iv1); 
-        vbox.setPadding(new Insets(10, 20, 10, 20));
-        sp.setPadding(new Insets(10, 20, 10, 20));
-        windowSp.setPadding(new Insets(1, 1, 1, 1)); 
-        vbox.getChildren().addAll(mb,sp);
-        vbox.prefWidthProperty().bind(primaryStage.widthProperty().subtract(50));
-        windowSp.setContent(vbox);
-        scene = new Scene(windowSp, 500, 500);
+        //make sure there is no previous data
+        sp.getChildrenUnmodifiable().remove(canvas);
+        sp.setContent(canvas);
+        //dont let scollpane get larger than canvas
+        sp.setMaxSize(imageWidth, imageHeight);
+        sp.autosize();
+      
+        //initialize toolBar and menuBar
+        ToolBarPain toolBar2 = new ToolBarPain();
+        toolBar2.setOrientation(Orientation.VERTICAL);
+        MenuPain mb = new MenuPain(primaryStage, gc, sp);
+
+        //initialize hbox
+        hbox = new HBox();
+        //make hbox fit canvas
+        hbox.setHgrow(sp, Priority.ALWAYS);
+        hbox.setFillHeight(true);
+        //sp holds canvas, arrange toolbar and canvas horizontally in hbox
+        hbox.getChildren().addAll(sp, toolBar2);
+        
+        //initialize vbox
+        vbox = new VBox();
+        vbox.setPadding(new Insets(1, 5, 1, 1));
+        //arrange menubar above canvas and toolbar in vbox
+        vbox.getChildren().addAll(mb,hbox);
+        //let vbox adjust to fit canvas
+        vbox.setVgrow(hbox, Priority.ALWAYS);
+        
+    
+        //conclude high level window initialization
+        Scene scene = new Scene(vbox, 500, 500);
         primaryStage.setTitle("Pain(t)");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
+    
+    
+    //method to intialize canvas, used by other classes when opening new images
+    
+    public static void canvasInit(Double imageW, Double imageH, Image img) {
+        
+        //remove prior data and initalize canvas
+        sp.getChildrenUnmodifiable().remove(canvas);
+        canvas = new Canvas(imageW, imageH);
+        gc = canvas.getGraphicsContext2D();
+        
+        //place image data on graphics context
+        gc.drawImage(img, 0, 0);
 
+        //built in padding for testing
+        sp.setMaxSize(imageW+20, imageH+20);
+        sp.setContent(canvas);
+    }
+       
 }
